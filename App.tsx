1812260b82a18bd8ea30f509474b79b19c739e9b
DariuszSikorra <dariusz.sikorra@gmail.com>
2020-03-21 22:07:59 +0100

    Validation semiworking

--- App.tsx
+++ App.tsx
@@ -3,32 +3,75 @@ import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const onSubmit = data => {
-    console.log(data)
+  const { register, handleSubmit, setValue, errors } = useForm({
+    validateCriteriaMode: "all"
+  });
 
+  const onSubmit = data => {
+    console.log(errors);
   };
-  const { register, handleSubmit, setValue } = useForm();
 
   useEffect(() => {
-    register("firstName");
-    register("password");
+    register(
+      { name: "email" },
+      {
+        required: "Email address is required",
+        pattern: {
+          value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          message: "It's not an email"
+        }
+      }
+    );
+
+    register(
+      { name: "password" },
+      {
+        required: "Password is required",
+        pattern: {
+          // value: /^(?=.*\d)[A-Za-z\d]{5,}$/,
+          value: /^(?=.*\d)[A-Za-z\d].*$/,
+          message: "Password require at least 1 digit."
+        },
+        minLength: { value: 6, message: "Password is to short" }
+      }
+    );
   }, [register]);
 
   return (
     <View style={styles.container}>
-      <Text>First Name</Text>
+      <Text>Email</Text>
       <TextInput
+        blurOnSubmit={true}
+        autoCapitalize="none"
+        placeholder="email"
+        textContentType="emailAddress"
         onChangeText={text => {
-          setValue("firstName", text);
+          setValue("email", text);
         }}
       />
+      {errors.email && errors.email.types.minLength && (
+        <Text>Email in not valid</Text>
+      )}
 
+      {/* password validation (min 6 chars, 1 digit) */}
       <Text>Password</Text>
       <TextInput
+        secureTextEntry={true}
+        autoCapitalize="none"
+        blurOnSubmit={false}
+        placeholder="password"
+        textContentType="password"
         onChangeText={text => {
-          setValue("password", text);
+          setValue("password", text, true);
         }}
       />
+      {/* {errors.password && <Text>{errors.password.message}</Text>} */}
+      {errors.password && errors.password.types.minLength && (
+        <Text>Password is to short</Text>
+      )}
+      {errors.password && errors.password.types.pattern && (
+        <Text>Password require at least 1 digit.</Text>
+      )}
 
       <Button title="submit" onPress={handleSubmit(onSubmit)} />
     </View>
